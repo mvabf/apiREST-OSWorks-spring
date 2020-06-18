201300effe7d3375c498b6c92434982b3d356c6c
Marcus Baltazar <mvabf_dev@outlook.com>
2020-06-18 10:56:08 -0300

    Projeto criado, CRUD cliente pronto, exceptions lidadas

---
+++ src/main/java/com/mvabf/osworks/domain/repository/ClienteRepository.java
@@ -0,0 +1,13 @@
+package com.mvabf.osworks.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mvabf.osworks.domain.model.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+	
+	Cliente findByEmail(String email);
+
+}
